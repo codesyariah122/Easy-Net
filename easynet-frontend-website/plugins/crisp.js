79196a9e97b88765a50624faa437e6801b743061
codesyariah122 <pujiermanto@gmail.com>
2021-12-30 23:18:50 +0700

    update crisp session  data setup

--- easynet-frontend-website/plugins/crisp.js
+++ easynet-frontend-website/plugins/crisp.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+// require('dotenv').config()
 export default () => {
   // const path = window.location.pathname.split('/')
 
 
   window.$crisp = [];
-  window.CRISP_WEBSITE_ID=process.env.CRISP_ID;
+  window.CRISP_WEBSITE_ID="c9cbc20c-cc22-4294-a59b-512eff53cc83";
   // var ask = prompt("Username anda : ")
 
   // console.log(`Halo ${ask}, apa kabarnya ? `)
